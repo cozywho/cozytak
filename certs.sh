2b2224936ebfd3d6b301e40c5ce3b368bb5bd042
cozy <156240279+cozywho@users.noreply.github.com>
2024-11-28 10:57:24 -0500

    Update certs.sh

--- certs.sh
+++ certs.sh
@@ -1,1 +1,22 @@
 
+CERTS.SH:
+modify the /opt/tak/certs/cert-metadata.sh file with default attributes
+
+
+cd/opt/tak/certs
+./makeRootCa.sh
+after user follows the prompts from makeRootCa.sh, generate a server cert
+./makeCert.sh server takserver
+./makeCert.sh client user
+./makeCert,sh client admin
+#leave tak user
+exit
+
+now in existing user, 
+
+sudo systemctl restart takserver
+sudo java -jar /opt/tak/utils/UserManager.jar certmod -A /opt/tak/certs/files/admin.pem
+
+modify the CoreConfig.xml
+
+
