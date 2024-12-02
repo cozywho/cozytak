43fda4607bfd3cb77692f9e72601de1c091b4917
cozy <156240279+cozywho@users.noreply.github.com>
2024-12-02 08:30:07 -0500

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -3,11 +3,11 @@
 echo "Starting "Existing Server Upgrade - One Server""
 
 # Install EPEL repository
-echo "Installing EPEL repository..."
+echo "Verifying EPEL repository..."
 sudo dnf install epel-release -y > /dev/null 2>&1
 
 # Add PostgreSQL repository and disable default module
-echo "Adding PostgreSQL repository..."
+echo "Verifying PostgreSQL repository..."
 sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm > /dev/null 2>&1
 sudo dnf -qy module disable postgresql > /dev/null 2>&1
 
@@ -16,11 +16,11 @@ echo "Updating the system (this may take a while)..."
 sudo dnf update -y > /dev/null 2>&1
 
 # Install Java
-echo "Installing Java..."
+echo "Verifying Java..."
 sudo dnf install java-17-openjdk-devel -y > /dev/null 2>&1
 
 # Enable powertools repository
-echo "Enabling powertools repository..."
+echo "Verifying powertools repository is enabled..."
 sudo dnf config-manager --set-enabled powertools > /dev/null 2>&1
 
 # Install TAK Server
