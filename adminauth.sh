0dd177fa69c3d5c69aeee3c381a717911ce4fb56
cozy <156240279+cozywho@users.noreply.github.com>
2024-12-19 10:29:37 -0500

    Update adminauth.sh

--- adminauth.sh
+++ adminauth.sh
@@ -1,1 +1,7 @@
 sudo java -jar /opt/tak/utils/UserManager.jar certmod -A /opt/tak/certs/files/admin.pem
+# Store script directory value
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Copy admin cert to cozytak folder for user to import.
+# sudo cp /opt/tak/certs/files/admin.pem "$SCRIPT_DIR/admin.pem" && sudo chown $(whoami):$(whoami) "$SCRIPT_DIR/admin.pem"
+sudo cp /opt/tak/certs/files/admin.p12 "$SCRIPT_DIR/admin.p12" && sudo chmod 777 "$SCRIPT_DIR/admin.p12" 
