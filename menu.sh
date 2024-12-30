98c38ab77e42a80e2d17858a9855433dc0f79a4c
cozy <156240279+cozywho@users.noreply.github.com>
2024-12-30 10:13:57 -0500

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -3,23 +3,52 @@
 # cozytak menu 
 echo
 echo "Welcome to cozytak. Get comfortable."
+
+# Check if TAK Server is running
+takserver_status=$(sudo systemctl is-active takserver 2>/dev/null)
+if [[ "$takserver_status" == "active" ]]; then
+    takserver_running=true
+    echo "TAK Server is currently running."
+else
+    takserver_running=false
+    echo "TAK Server is not running."
+fi
+
 # Determine the directory the script is being run from
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "Your cozytak directory is: $SCRIPT_DIR"
 echo "-----------------------------------"
 echo "Select an option:"
 echo "-----------------------------------"
-echo "1. New Install - One Server"
-echo "2. Upgrade existing TAK server"
+
+option_num=1
+
+if [[ "$takserver_running" == false ]]; then
+    echo "$option_num. New Install - One Server"
+    install_option=$option_num
+    ((option_num++))
+fi
+
+echo "$option_num. Upgrade existing TAK server"
+upgrade_option=$option_num
+((option_num++))
+
+echo "$option_num. Certificate Generator (Drag & Drop)"
+certgen_option=$option_num
+((option_num++))
+
+echo "$option_num. Exit"
+exit_option=$option_num
+
 echo "-----------------------------------"
-echo "3. Certificate Generator (Drag & Drop)"
-echo "-----------------------------------"
-echo "4. Exit"
-echo "-----------------------------------"
-read -p "Enter your choice (1/2/3/4): " choice
+read -p "Enter your choice: " choice
 
 case $choice in
-    1)
+    $install_option)
+        if [[ "$takserver_running" == true ]]; then
+            echo "Invalid option. New Install is not available because TAK Server is already running."
+            exit 1
+        fi
         read -p "Are you ready to proceed? (y/n): " ready
 
         if [[ "${ready,,}" != "y" ]]; then
@@ -34,7 +63,7 @@ case $choice in
             exit 1
         fi
         ;;
-    2)
+    $upgrade_option)
         echo "Executing TAK Server Upgrade..."
         if [[ -f "$SCRIPT_DIR/upgrade.sh" ]]; then
             source "$SCRIPT_DIR/upgrade.sh"
@@ -43,7 +72,7 @@ case $choice in
             exit 1
         fi
         ;;
-    3)
+    $certgen_option)
         echo "Cert Generation..."
         if [[ -f "$SCRIPT_DIR/certgen.sh" ]]; then
             source "$SCRIPT_DIR/certgen.sh"
@@ -52,7 +81,7 @@ case $choice in
             exit 1
         fi
         ;;
-    4)
+    $exit_option)
         echo "Exiting."
         exit 0
         ;;
