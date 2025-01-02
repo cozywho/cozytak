73dccfac6046ad2aff06259935b04a07aca8707e
cozy <156240279+cozywho@users.noreply.github.com>
2025-01-02 14:22:27 -0500

    Update certgen.sh

--- certgen.sh
+++ certgen.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
+
 # Prompt for username
 read -p "Enter username for the cert: " USERNAME
+
 # Check for duplicate username
-    if [ -e "/opt/tak/certs/files/$USERNAME.p12" ]; then
-        echo "A certificate for '$USERNAME' already exists. Please choose another username."
-    else
-        break
-    fi
-done
+if [ -e "/opt/tak/certs/files/$USERNAME.p12" ]; then
+    echo "A certificate for '$USERNAME' already exists. Please choose another username."
+    exit 1
+fi
 
 # Generate the certificate
-sudo -u tak ./opt/tak/certs/makeCert.sh "$USERNAME"
+sudo -u tak /opt/tak/certs/makeCert.sh "$USERNAME"
 
 # Check if the .p12 file was successfully created
 if [ ! -e "/opt/tak/certs/files/$USERNAME.p12" ]; then
@@ -18,68 +18,64 @@ if [ ! -e "/opt/tak/certs/files/$USERNAME.p12" ]; then
     exit 1
 fi
 
-mkdir /opt/cozytak/certs/$USERNAME
+# Create the user's certificate directory
+USER_CERT_DIR="/opt/cozytak/certs/$USERNAME"
+mkdir -p "$USER_CERT_DIR"
 
 # Copy the required files to the user's folder
-touch 
-sudo cp "/opt/tak/certs/files/$USERNAME.p12" "/opt/cozytak/certs/$USERNAME/"
-sudo cp "/opt/tak/certs/files/truststore-root.p12" "/opt/cozytak/certs/$USERNAME/"
-touch /opt/cozytak/certs/$USERNAME/manifest.xml
-touch /opt/cozytak/certs/$USERNAME/package_builder.pref
+sudo cp "/opt/tak/certs/files/$USERNAME.p12" "$USER_CERT_DIR/user.p12"
+sudo cp "/opt/tak/certs/files/truststore-root.p12" "$USER_CERT_DIR/"
+touch "$USER_CERT_DIR/manifest.xml" "$USER_CERT_DIR/package_builder.pref"
 
-#manifest.xml
-manifest="/opt/cozytak/certs/$USERNAME/manifest.xml"
-{
-    echo "<?xml version=\"1.0\" standalone=\"yes\"?>"
-    echo "<MissionPackageManifest version=\"2\">"
-    echo "  <Configuration>"
-    echo "    <Parameter name=\"name\" value=\"localhost\" />"
-    echo "    <Parameter name=\"uid\" value=\"78f3ce32-2d11-4b6e-a668-77551aa081f6\" />"
-    echo "  </Configuration>"
-    echo "  <Contents>"
-    echo "    <Content zipEntry=\"user.p12\" ignore=\"false\" />"
-    echo "    <Content zipEntry=\"truststore-root.p12\" ignore=\"false\" />"
-    echo "    <Content zipEntry=\"package_builder.pref\" ignore=\"false\" />"
-    echo "  </Contents>"
-    echo "</MissionPackageManifest>"
-} > "$conf_file"
+# Create manifest.xml
+manifest="$USER_CERT_DIR/manifest.xml"
+cat <<EOF > "$manifest"
+<?xml version="1.0" standalone="yes"?>
+<MissionPackageManifest version="2">
+  <Configuration>
+    <Parameter name="name" value="localhost" />
+    <Parameter name="uid" value="78f3ce32-2d11-4b6e-a668-77551aa081f6" />
+  </Configuration>
+  <Contents>
+    <Content zipEntry="user.p12" ignore="false" />
+    <Content zipEntry="truststore-root.p12" ignore="false" />
+    <Content zipEntry="package_builder.pref" ignore="false" />
+  </Contents>
+</MissionPackageManifest>
+EOF
 
-#variables
-$HOSTNAME
-$IPADDY
-$USERNAME
-$PASSWORD
+# Define variables (replace these with actual values or ensure they are set beforehand)
+HOSTNAME="example-host"
+IPADDY="192.168.1.1"
+PASSWORD="example-password"
 
-#packagebuilder.pref
-package_builder="/opt/cozytak/certs/$USERNAME/package_builder.pref"
-{
-    echo "<?xml version='1.0' standalone='yes'?>"
-    echo "<preferences>"
-    echo "<preference version=\"1\" name=\"cot_streams\">"
-    echo "<entry key=\"count\" class=\"class java.lang.Integer\">1</entry>"
-    echo "<entry key=\"description0\" class=\"class java.lang.String\">$HOSTNAME</entry>"
-    echo "<entry key=\"enabled0\" class=\"class java.lang.Boolean\">true</entry>"
-    echo "<entry key=\"connectString0\" class=\"class java.lang.String\">$IPADDY:8089:ssl</entry>"
-    echo "</preference>"
-    echo "<preference version=\"1\" name=\"com.atakmap.app_preferences\">"
-    echo "<entry key=\"caLocation\" class=\"class java.lang.String\">cert/truststore-root.p12</entry>"
-    echo "<entry key=\"certificateLocation\" class=\"class java.lang.String\">cert/$USERNAME.p12</entry>"
-    echo "<entry key=\"clientPassword\" class=\"class java.lang.String\">$PASSWORD</entry>"
-    echo "<entry key=\"caPassword\" class=\"class java.lang.String\">$PASSWORD</entry>"
-    echo "<entry key=\"displayServerConnectionWidget\" class=\"class java.lang.Boolean\">true</entry>"
-    echo "</preference>"
-    echo "<preference version=\"1\" name=\"mil.afrl.risd.android.ATAK_preferences\">"
-    echo "</preference>"
-    echo "</preferences>"
-} > "$conf_file"
-
-
+# Create package_builder.pref
+package_builder="$USER_CERT_DIR/package_builder.pref"
+cat <<EOF > "$package_builder"
+<?xml version="1.0" standalone="yes"?>
+<preferences>
+  <preference version="1" name="cot_streams">
+    <entry key="count" class="class java.lang.Integer">1</entry>
+    <entry key="description0" class="class java.lang.String">$HOSTNAME</entry>
+    <entry key="enabled0" class="class java.lang.Boolean">true</entry>
+    <entry key="connectString0" class="class java.lang.String">$IPADDY:8089:ssl</entry>
+  </preference>
+  <preference version="1" name="com.atakmap.app_preferences">
+    <entry key="caLocation" class="class java.lang.String">cert/truststore-root.p12</entry>
+    <entry key="certificateLocation" class="class java.lang.String">cert/user.p12</entry>
+    <entry key="clientPassword" class="class java.lang.String">$PASSWORD</entry>
+    <entry key="caPassword" class="class java.lang.String">$PASSWORD</entry>
+    <entry key="displayServerConnectionWidget" class="class java.lang.Boolean">true</entry>
+  </preference>
+  <preference version="1" name="mil.afrl.risd.android.ATAK_preferences" />
+</preferences>
+EOF
 
 # Create a zip file of the user's cert folder
-ZIP_FILE="$COZYTAK_CERTS_DIR/$USERNAME.zip"
-zip -r "/opt/cozytak/certs/$USERNAME.zip" "/opt/cozytak/certs/$USERNAME"
+ZIP_FILE="/opt/cozytak/certs/$USERNAME.zip"
+zip -r "$ZIP_FILE" "$USER_CERT_DIR" >/dev/null
 
-
+# Final output
 echo "--------------------------------------------------"
 echo "$USERNAME package created in /opt/cozytak/certs."
 echo "--------------------------------------------------"
@@ -91,4 +87,3 @@ else
     echo "Error: Failed to create certificate package."
     exit 1
 fi
-
